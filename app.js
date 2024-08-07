02af30b18087c121bd14a0b2d2edac470790f6ae
Keita Iijima <koka3903@gmail.com>
2024-08-07 14:02:35 +0900

    Set up Express and MongoDB connection

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    .then( () => {
+        console.log('MongoDBコネクションOK!!');
+    })
+    .catch( (e) => {
+        console.log('MongoDBコネクションエラー!!');
+        console.log('e');
+    });
+    
+const app = express();
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')); //res.renderの際に動作する
 
 app.get('/', (req, res) => {
     res.render('home');
+});
+app.get('/make', async (req, res) => {
+    const camp = new Campground({ title: '私の庭', description: '気軽に安くキャンプ！！'});
+    await camp.save();
+    res.send(camp);
 });
 
 app.listen('3000', () => {
