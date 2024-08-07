7b36be9294a88645e59926e5710a85e4d9642208
Keita Iijima <koka3903@gmail.com>
2024-08-07 19:42:56 +0900

    Seed data setting completed

--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const cities = require('./cities');
+const { descriptors, places } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
@@ -10,10 +12,21 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {useNewUrlParser: true, 
         console.log('e');
     });
 
-const seedDB =async () => {
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+// ランダムに50個、citiesからlocation,cityを取得してくる
+const seedDB = async () => {
     await Campground.deleteMany({});
-    const c = new Campground({title: 'オートキャンプ'});
-    await c.save();
+    for(let i = 0; i < 50; i++) {
+        const randomCityIndex = Math.floor(Math.random() * cities.length);
+        const camp = new Campground({
+            location: `${cities[randomCityIndex].prefecture}${cities[randomCityIndex].city}`,
+            title: `${sample(descriptors)}・${sample(places)}`
+        });
+        await camp.save();
+    }
 }
 
-seedDB();
+seedDB().then(() => {
+    mongoose.connection.close();
+});
