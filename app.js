6900513aba347395b9cd0912fd0b83134b71d2c7
Keita Iijima <koka3903@gmail.com>
2024-09-30 20:52:40 +0900

    rename const

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
-const morgan = require('morgan')
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true ,useFindAndModify: false})
@@ -16,14 +16,13 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {useNewUrlParser: true, 
     
 const app = express();
 
-
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')); //res.renderの際に動作する
 
 // formのリクエストをparseする
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
-app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
     res.render('home');
